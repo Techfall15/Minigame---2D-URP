c53bd563bdf74486a1f077db77d0a359935e3dd3
Techfall15 <102117551+Techfall15@users.noreply.github.com>
2024-04-24 22:47:27 -0400

    Created the grid shader preview for the inspector.

--- Assets/Editor/StarSystemEditor.cs
+++ Assets/Editor/StarSystemEditor.cs
@@ -1,6 +1,11 @@
 using UnityEditor;
+using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEditor.UIElements;
+using System.Collections.Generic;
+using System;
+using System.Xml;
+using System.Net.NetworkInformation;
+
 
 
 [CustomEditor(typeof(StarSystem))]
@@ -8,28 +13,253 @@ public class StarSystemEditor : Editor
 {
     public VisualTreeAsset m_treeAsset;
     private StarSystem m_starSystem;
-
+    private Label m_spawnPosLimitLabel;
+    private Vector2Field m_spawnPositionLimit;
+    private Vector2Field m_spawnPositionLimit2;
+    private Button spawnStarsBtn;
+    private Button clearBtn;
+    private Toggle customizeSpawnToggle;
+    private DropdownField spawnDirectionField;
+    
+    private List<Vector2> gridIndexes;
+    private int rowIndex;
+    private int colIndex;
+    private int row2Index;
+    private int col2Index;
+    private HashSet<VisualElement> m_coloredPoints;
     private void OnEnable()
     {
         m_starSystem = (StarSystem)target;
+        
     }
     public override VisualElement CreateInspectorGUI()
     {
         var root = new VisualElement();
+        m_coloredPoints = new HashSet<VisualElement>();
         m_treeAsset.CloneTree(root);
 
-        Button spawnStarsBtn = root.Q<Button>("spawnStarsBtn");
-        spawnStarsBtn.RegisterCallback<ClickEvent>(OnSpawnStarsButtonClick);
-        Button clearBtn = root.Q<Button>("clearAllStarsBtn");
-        clearBtn.RegisterCallback<ClickEvent>(OnClearAllStarsButtonClick);
+        InitializeElements(root);
+        RegisterCallbacks(root);
+
+        if (customizeSpawnToggle.value == false) m_spawnPosLimitLabel.SetEnabled(true);
+        
+        spawnDirectionField.SetEnabled(customizeSpawnToggle.value);
+        var choices = new List<string> { "As Above", "So Below"};
+        spawnDirectionField.choices = choices;
+        spawnDirectionField.value = choices[0];
+
+
+        
         
         return root;
     }
+    #region Initialize Elements
+    private void InitializeElements(VisualElement root)
+    {
+        m_spawnPosLimitLabel = root.Q<Label>("spawnPosLimitLabel");
+        m_spawnPositionLimit = root.Q<Vector2Field>("spawnPos1");
+        m_spawnPositionLimit2 = root.Q<Vector2Field>("spawnPos2");
+
+        spawnStarsBtn = root.Q<Button>("spawnStarsBtn");
+        clearBtn = root.Q<Button>("clearAllStarsBtn");
+        customizeSpawnToggle = root.Q<Toggle>("customizeSpawnToggle");
+        spawnDirectionField = root.Q<DropdownField>("spawnDirectionDropField");
+        m_spawnPosLimitLabel.SetEnabled(customizeSpawnToggle.value);
+        gridIndexes = GetGridIndexes(root);
+
+        rowIndex = GetTargetRow(m_spawnPositionLimit.value.x);
+        colIndex = GetTargetCol(m_spawnPositionLimit.value.y);
+        row2Index = GetTargetRow(m_spawnPositionLimit2.value.x);
+        col2Index = GetTargetCol(m_spawnPositionLimit2.value.y);
+        m_coloredPoints.Clear();
+        m_coloredPoints.Add(root.Q<VisualElement>(rowIndex.ToString() + colIndex.ToString()));
+        m_coloredPoints.Add(root.Q<VisualElement>(row2Index.ToString() + col2Index.ToString()));
+
+        foreach (var point in m_coloredPoints) { point.style.backgroundColor = Color.red; }
+    }
+    #endregion
+    
+    #region Register Callbacks
+    private void RegisterCallbacks(VisualElement root)
+    {
+        m_spawnPositionLimit.RegisterValueChangedCallback(evt =>
+        {
+            foreach(var child in root.Q<VisualElement>("spawnAreaGroup").Children())
+            {
+                child.style.backgroundColor = Color.clear;
+            }
+            OnSpawnLimit1Change(evt, root);
+            
+        });
+        m_spawnPositionLimit2.RegisterValueChangedCallback(evt =>
+        {
+            foreach (var child in root.Q<VisualElement>("spawnAreaGroup").Children())
+            {
+                child.style.backgroundColor = Color.clear;
+            }
+            OnSpawnLimit2Change(evt, root);
+            
+        });
+        spawnStarsBtn.RegisterCallback<ClickEvent>(OnSpawnStarsButtonClick);
+        clearBtn.RegisterCallback<ClickEvent>(OnClearAllStarsButtonClick);
+        
+        customizeSpawnToggle.RegisterValueChangedCallback(evt =>
+        {
+            spawnDirectionField.SetEnabled(evt.newValue);
+            m_spawnPosLimitLabel.text = (evt.newValue == true) ? (spawnDirectionField.value == "As Above") ?
+                "Spawns Stars Above (x,y)" : "Spawns Stars Below (x,y)" :
+                "Spawns Within (-x,x) and (-y,y)";
+            
+            OnCustomizeSpawnChange(evt);
+        });
+        spawnDirectionField.RegisterCallback<ChangeEvent<string>>((evt) =>
+        {
+            OnAsAboveSoBelowChange(evt, root);
+            m_spawnPosLimitLabel.text = (evt.newValue == "As Above") ? "Spawns Stars Above (x,y)" : "Spawns Stars Below (x,y)";
+        });
+    }
+    #endregion
 
     #region Button Events
 
     private void OnSpawnStarsButtonClick(ClickEvent evt) => m_starSystem.SpawnStars();
     private void OnClearAllStarsButtonClick(ClickEvent evt) => m_starSystem.DisableAllStars();
+    private void OnCustomizeSpawnChange(ChangeEvent<bool> evt)
+    {
+        m_starSystem.SetCustomizeSpawnTo(evt.newValue);
+    }
+    private void OnAsAboveSoBelowChange(ChangeEvent<string> evt, VisualElement root)
+    {
+        if(evt.newValue == "As Above")
+        {
+            m_starSystem.ToggleAsAboveSoBelow(StarSystem.m_spawnState.OnlySpawnAbove);
+            PopulateColoredList(root, true);
+        }
+        else
+        {
+            m_starSystem.ToggleAsAboveSoBelow(StarSystem.m_spawnState.OnlySpawnBelow);
+            PopulateColoredList(root, false);
+        }
+    }
 
     #endregion
+
+    #region Grid Events
+    private List<Vector2> GetGridIndexes(VisualElement root)
+    {
+        var indexes = new List<Vector2>();
+        var points = new List<VisualElement>();
+        var grid = root.Q<VisualElement>("spawnAreaGroup");
+        for (int i = 0; i < grid.childCount; i++)
+        {
+            var newPoint = grid.ElementAt(i);
+            if (i < 10) newPoint.name = "0" + i.ToString();
+            else newPoint.name = i.ToString();
+            points.Add(newPoint);
+        }
+        for (int i = 0; i < points.Count; i++)
+        {
+            if (i < 10) indexes.Add(new Vector2(0, int.Parse(points[i].name)));
+            else indexes.Add(new Vector2(int.Parse(points[i].name[0].ToString()), int.Parse(points[i].name[1].ToString())));
+        }
+        return indexes;
+    }
+    private void OnSpawnLimit1Change(ChangeEvent<Vector2> evt, VisualElement root)
+    {
+        colIndex = GetTargetCol(evt.newValue.x);
+        rowIndex = GetTargetRow(evt.newValue.y);
+
+        var aboveBelow = spawnDirectionField.value == "As Above";
+        PopulateColoredList(root, aboveBelow);
+
+
+        m_coloredPoints.Add(root.Q<VisualElement>(rowIndex.ToString()+colIndex.ToString()));
+        m_coloredPoints.Add(root.Q<VisualElement>(row2Index.ToString() + col2Index.ToString()));
+        
+        foreach (var point in m_coloredPoints) { point.style.backgroundColor = Color.red; }
+    }
+    private void OnSpawnLimit2Change(ChangeEvent<Vector2> evt, VisualElement root)
+    {
+        col2Index = GetTargetCol(evt.newValue.x);
+        row2Index = GetTargetRow(evt.newValue.y);
+
+        var aboveBelow = spawnDirectionField.value == "As Above";
+        PopulateColoredList(root, aboveBelow);
+        
+
+        m_coloredPoints.Add(root.Q<VisualElement>(rowIndex.ToString() + colIndex.ToString()));
+        m_coloredPoints.Add(root.Q<VisualElement>(row2Index.ToString() + col2Index.ToString()));
+
+        foreach (var point in m_coloredPoints) { point.style.backgroundColor = Color.red; }
+    }
+    private int GetTargetCol(float x)
+    {
+        var xPoint = x;
+        var xMod = (xPoint >= 1) ? 0:1;
+        var xPointString = xPoint.ToString("0.00");
+        var xSplit = xPointString.ToString().Split('.');
+        var xIntString = (xPoint < 0) ? xSplit[0].ToString().Substring(0, 2) : xSplit[0].ToString().Substring(0, 1);
+        var xInt = int.Parse(xIntString.ToString());
+        return 4 + xMod + xInt;
+    }
+    private int GetTargetRow(float y)
+    {
+        var yPoint = y;
+        var yMod = (yPoint < 0) ? 1 : 0;
+        
+        var yPointString = yPoint.ToString("0.00");
+        var ySplit = yPointString.ToString().Split('.');
+        var yIntString = (yPoint < 0) ? ySplit[0].ToString().Substring(0, 2) : ySplit[0].ToString().Substring(0, 1);
+        var yInt = int.Parse(yIntString.ToString()) * -1;
+        if (yPoint <= -1) yInt = yInt - 1;
+        if (yPoint >= 1) yInt = yInt + 1;
+        return 4 + yMod + yInt;
+    }
+    private void PopulateColoredList(VisualElement root, bool aboveBelow)
+    {
+        m_coloredPoints.Clear();
+        var slope = (m_spawnPositionLimit2.value.y - m_spawnPositionLimit.value.y) / (m_spawnPositionLimit2.value.x- m_spawnPositionLimit.value.x);
+        var yIntercept = m_spawnPositionLimit2.value.y - (slope * m_spawnPositionLimit2.value.x);
+        Debug.Log("Slope of line is: " + slope);
+        Debug.Log("Which is: " + (m_spawnPositionLimit2.value.y - m_spawnPositionLimit.value.y) + " / " + (m_spawnPositionLimit2.value.x - m_spawnPositionLimit.value.x) + " after being simplified");
+        Debug.Log("Y-Intercept of line is: " + yIntercept);
+
+        m_coloredPoints.Clear();
+        foreach (var point in root.Q<VisualElement>("spawnAreaGroup").Children())
+        {
+            var x3 = int.Parse(point.name[1].ToString());
+            var y3 = int.Parse(point.name[0].ToString());
+            if (point.name == rowIndex.ToString() + colIndex.ToString() || point.name == row2Index.ToString() + col2Index.ToString())
+            {
+                m_coloredPoints.Add(point);
+            }
+            x3 = x3 - 5;
+            y3 = 5 - y3;
+
+            var newIntercept = y3 - (slope * x3);
+
+            if(aboveBelow == true)
+            {
+                if (newIntercept >= yIntercept) m_coloredPoints.Add(point);
+                
+            }
+            else
+            {
+                if (newIntercept <= yIntercept) m_coloredPoints.Add(point);
+                
+            }
+
+        }
+        root.MarkDirtyRepaint();
+    }
+    #endregion
+
+
+
+
+
+   
+
 }
+
+
--- Assets/Scripts/StarSystem.cs
+++ Assets/Scripts/StarSystem.cs
@@ -3,25 +3,37 @@ using UnityEngine.UIElements;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 
 
-public class StarSystem: MonoBehaviour
+public class StarSystem : MonoBehaviour
 {
 
     [SerializeField] private int m_spawnAmount = 10;
     [SerializeField] private float m_starScale = 1f;
     [SerializeField] private float m_starSpeed = 1f;
     [SerializeField] private Vector2 m_spawnPositionLimit = new Vector2(8.5f, 4.5f);
+    [SerializeField] private Vector2 m_spawnPositionLimit2 = new Vector2(8.5f, 4.5f);
     [SerializeField] private Star[] m_starList = new Star[5];
     [SerializeField] private List<Color> m_colorList = new List<Color>() {
         Color.white, Color.blue, Color.red, Color.yellow, Color.green};
     [SerializeField] private GameObject m_prefab;
-    
+    [SerializeField] private bool m_customizeSpawn = false;
+    [SerializeField] private bool m_onlySpawnAbove = false;
+    [SerializeField] private bool m_onlySpawnBelow = false;
+    public enum m_spawnState { OnlySpawnAbove, OnlySpawnBelow };
+    private m_spawnState spawnState;
 
+    private void Awake()
+    {
+        spawnState = (m_customizeSpawn) ? m_spawnState.OnlySpawnAbove : m_spawnState.OnlySpawnBelow;
+    }
 
     private void Start()
     {
         SpawnStars();
+        if(m_customizeSpawn == true) Debug.Log("Stars are using custom spawn");
+        else Debug.Log("Stars are not using custom spawn");
     }
 
     public void SpawnStars()
@@ -32,7 +44,15 @@ public class StarSystem: MonoBehaviour
             if (star != null)
             {
                 StarController starController = star.GetComponent<StarController>();
-                RandomizeStar(starController);
+                if(m_customizeSpawn == true)
+                {
+                    if(spawnState == m_spawnState.OnlySpawnAbove)
+                    {
+                        RandomizeStar(starController);
+                        CustomizeSpawn(starController, m_spawnPositionLimit, spawnState);
+                    }
+                }
+                else RandomizeStar(starController);
                 star.SetActive(true);
 
             }
@@ -46,17 +66,67 @@ public class StarSystem: MonoBehaviour
             star.SetActive(false);
         }
     }
-    
+
+    #region Randomize Functions
     private void RandomizeStar(StarController star)
     {
-        star.m_starClass.SetScaleTo(Random.Range(0,m_starScale));
-        star.m_starClass.SetSpeedTo(Random.Range(0.1f, m_starSpeed));
-        star.m_starClass.SetColorTo(m_colorList[Random.Range(0, m_colorList.Count)]);
-        star.m_starClass.SetSpawnPositionTo(new Vector2(
+        RandomizeScale(star);
+        RandomizeSpeed(star);
+        RandomizeColor(star);
+        RandomizeSpawn(star);
+    }
+    private void RandomizeScale(StarController star) => star.m_starClass.SetScaleTo(Random.Range(0, m_starScale));
+    private void RandomizeSpeed(StarController star) => star.m_starClass.SetSpeedTo(Random.Range(0.1f, m_starSpeed));
+    private void RandomizeColor(StarController star) => star.m_starClass.SetColorTo(m_colorList[Random.Range(0, m_colorList.Count)]);
+    private void RandomizeSpawn(StarController star) => star.m_starClass.SetSpawnPositionTo(new Vector2(
             Random.Range(-m_spawnPositionLimit.x, m_spawnPositionLimit.x),
             Random.Range(-m_spawnPositionLimit.y, m_spawnPositionLimit.y)));
-        
-    }
-    
+    #endregion
 
+    #region Customize Functions
+    private void CustomizeSpawn(StarController star, Vector2 limits, m_spawnState state)
+    {
+        switch (state)
+        {
+            case m_spawnState.OnlySpawnBelow:
+                star.m_starClass.SetSpawnPositionTo(new Vector2(
+                    Random.Range(-9f, limits.x),
+                    Random.Range(-5f, limits.y)));
+                break;
+            case m_spawnState.OnlySpawnAbove:
+                star.m_starClass.SetSpawnPositionTo(new Vector2(
+                    Random.Range(limits.x, 9f),
+                    Random.Range(limits.y, 5f)));
+                break;
+            default:
+                Debug.Log("Error customizeing spawn position");
+                break;
+        }
+        
+
+    }    
+    #endregion
+    public void ToggleAsAboveSoBelow(m_spawnState state)
+    {
+        switch (state)
+        {
+            case m_spawnState.OnlySpawnAbove:
+                m_onlySpawnAbove = true;
+                m_onlySpawnBelow = false;
+                Debug.Log("Changed to As Above");
+
+                break;
+            case m_spawnState.OnlySpawnBelow:
+                m_onlySpawnAbove = false;
+                m_onlySpawnBelow = true;
+                Debug.Log("Changed to So Below");
+
+                break;
+            default:
+                Debug.Log("Error toggling above or below spawner");
+                break;
+        }
+    }
+    public void SetCustomizeSpawnTo(bool state) => m_customizeSpawn = state;
+    
 }

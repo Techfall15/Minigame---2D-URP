454bbba2e853452821e370348933ac003bd1aa37
Techfall15 <102117551+Techfall15@users.noreply.github.com>
2024-05-03 16:52:01 -0400

    Added rotation to the tree particles that spawn when the player swings the axe.

--- Assets/Scripts/PlayerChopController.cs
+++ Assets/Scripts/PlayerChopController.cs
@@ -9,6 +9,7 @@ public class PlayerChopController : MonoBehaviour
 
     [SerializeField] private float m_currentPower = 0f;
     [SerializeField] private GameObject m_playerPivot;
+    [SerializeField] private ParticleSystem logParticles;
     [SerializeField] private SpriteRenderer m_spriteRenderer;
     [SerializeField] private Sprite[] m_spriteArray = new Sprite[3];
     [SerializeField] private Vector3 m_xPivotStateValues = new Vector3(0f, -0.25f, 0.25f);
@@ -29,27 +30,21 @@ public class PlayerChopController : MonoBehaviour
     }
     private void Start()
     {
-        if(m_spriteRenderer != null) InitializeSwingStateValues(m_currentSwingState);
-    }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            AdvanceSwingState();
-        }
+        if(m_spriteRenderer != null) SetSwingStateValuesFor(m_currentSwingState);
     }
     public void StartRevertToDefault()
     {
         StopCoroutine("RevertToDefault");
         AdvanceSwingState();
         StartCoroutine("RevertToDefaultCoroutine", 0.5f);
+        // Get reference to the log to start particles. Eventually make an event.
+        logParticles.Play();
     }
     
     public void AdvanceSwingState()
     {
-        IncrementSwingState(m_currentSwingState);
-        Debug.Log("m_currentSwingState changed to: " + m_currentSwingState);
-        InitializeSwingStateValues(m_currentSwingState);
+        SetNextSwingState((int)m_currentSwingState);
+        SetSwingStateValuesFor(m_currentSwingState);
     }
     private IEnumerator RevertToDefaultCoroutine(float delay)
     {
@@ -65,38 +60,24 @@ public class PlayerChopController : MonoBehaviour
         m_currentPower += amount;
         Debug.Log("current Power is: " + m_currentPower);
     }
-    private void InitializeSwingStateValues(m_swingState currentState)
-    {
-        var pivotTransform = m_playerPivot.transform;
-        var pivotPos = pivotTransform.localPosition;
-        
-        switch (currentState)
-        {
-            case m_swingState.BeforeChop:
-                pivotTransform.localPosition = new Vector3(m_xPivotStateValues[0], m_yOffsetStateValues[0], pivotPos.z);
-                pivotTransform.localRotation = Quaternion.Euler(0f, 0f, m_zRotationStateValues[0]);
-                m_spriteRenderer.sprite = m_spriteArray[(int)currentState];
-                break;
-            case m_swingState.DuringChop:
-                pivotTransform.localPosition = new Vector3(m_xPivotStateValues[1], m_yOffsetStateValues[1], pivotPos.z);
-                pivotTransform.localRotation = Quaternion.Euler(0f, 0f, m_zRotationStateValues[1]);
-                m_spriteRenderer.sprite = m_spriteArray[(int)currentState];
-                break;
-            case m_swingState.AfterChop:
-                pivotTransform.localPosition = new Vector3(m_xPivotStateValues[2], m_yOffsetStateValues[2], pivotPos.z);
-                pivotTransform.localRotation = Quaternion.Euler(0f, 0f, m_zRotationStateValues[2]);
-                m_spriteRenderer.sprite = m_spriteArray[(int)currentState];
-                break;
-            default:
-                Debug.Log("Error Initializing Swing State Values");
-                break;
-        }
-    }
-    private void IncrementSwingState(m_swingState currentState)
+    private void SetSwingStateValuesFor(m_swingState currentState)
     {
         var state = (int)currentState;
+        SetPivotTransform(state);
+        m_spriteRenderer.sprite = m_spriteArray[state];
+    }
+    private void SetNextSwingState(int state)
+    {
         if (state == 0) m_currentSwingState = m_swingState.DuringChop;
         else if (state == 1) m_currentSwingState = m_swingState.AfterChop;
         else m_currentSwingState = m_swingState.BeforeChop;
+    }
+    private void SetPivotTransform(int state)
+    {
+        var pivotTransform = m_playerPivot.transform;
+        var pivotPos = pivotTransform.localPosition;
+
+        pivotTransform.localPosition = new Vector3(m_xPivotStateValues[state], m_yOffsetStateValues[state], pivotPos.z);
+        pivotTransform.localRotation = Quaternion.Euler(0f, 0f, m_zRotationStateValues[state]);
     }
 }
